fc481e286226f93ea29666abb6e3a8911adac622
Sonwanseo <sonwanseo@gmail.com>
2021-01-28 16:19:03 +0900

    chapter 10-3

--- velopertExpress/chapter 9/router/main.js
+++ velopertExpress/chapter 9/router/main.js
@@ -1,8 +1,12 @@
 module.exports = function (app, fs) {
     app.get('/', function (req, res) {
+        const sess = req.session;
+
         res.render('index', {
             title: "MY HOMEPAGE",
-            length: 5
+            length: 5,
+            name: sess.name,
+            username: sess.username
         })
     });
 
@@ -105,5 +109,51 @@ module.exports = function (app, fs) {
                 return;
             })
         })
+    })
+
+    app.get('/login/:username/:password', function (req, res) {
+        const sess = req.session;
+
+        fs.readFile(`${__dirname}/../data/user.json`, "utf8", function (err, data) {
+            const users = JSON.parse(data);
+            const username = req.params.username;
+            const password = req.params.password;
+            let result = {};
+
+            // Username Not Found
+            if (!users[username]) {
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if (users[username]["password"] == password) {
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            } else {
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    })
+
+    app.get('/logout', function (req, res) {
+        sess = req.session;
+
+        if (sess.username) {
+            req.session.destroy(function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect('/');
+                }
+            })
+        } else {
+            res.redirect('/');
+        }
     })
 };
